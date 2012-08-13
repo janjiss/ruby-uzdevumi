e9e64aef1cf657f213c2dac9e62860a85e8202ca
Lauris <lauris@lauris-desktop.(none)>
2012-08-13 14:46:26 +0300

    Fixed grammer third task

--- 3-files/weather.rb
+++ 3-files/weather.rb
@@ -1,6 +1,10 @@
-class Weather
-  def initialize(file_url)
+require "nokogiri"
 
+class Weather
+  attr_reader :file, :nok
+  def initialize(file_url)
+    @file = open(file_url)
+    @nok = Nokogiri::XML(@file)
   end
-  
+
 end
